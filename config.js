0e0f8c7904967eebb0373caff7fe5cb27ca69852
Peter Holmberg <peterholmberg@users.noreply.github.com>
2017-03-07 16:05:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,5 @@ module.exports = {
    port: process.env.PORT || 3000,
    base_url: process.env.BASE_URL || 'http://localhost:3000',
    db: {
-      // uri: 'mongodb://127.0.0.1:27017/api',
-      uri: 'mongodb://pistamalmo:gaylords@ds135069.mlab.com:35069/heroku_sdjx8wg1'
-   },
+      uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/api'
 };
