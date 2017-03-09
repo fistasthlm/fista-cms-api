34cf4073023abd7ed49d66f7f683982a8e822d66
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-03-09 16:30:34 +0100

    more explicit vars

--- models/bike.js
+++ models/bike.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var mongoose = require('mongoose'),
-   mongooseApiQuery = require('mongoose-api-query'),
-   createdModified = require('mongoose-createdmodified').createdModifiedPlugin;
+var mongoose = require('mongoose');
+var mongooseApiQuery = require('mongoose-api-query');
+var createdModified = require('mongoose-createdmodified').createdModifiedPlugin;
 
 var BikeSchema = new mongoose.Schema({
    title: String,
