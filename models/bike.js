92055a1c33be6abd7dddd3093a172ef62b85f3da
Peter Holmberg <peter.hlmbrg@gmail.com>
2017-12-23 20:45:33 +0100

    started work

--- models/bike.js
+++ models/bike.js
@@ -5,26 +5,30 @@ var mongooseApiQuery = require('mongoose-api-query');
 var createdModified = require('mongoose-createdmodified').createdModifiedPlugin;
 
 var BikeSchema = new mongoose.Schema({
-   title: String,
-   frame: String,
-   fork: String,
-   cranks: String,
-   pedals: String,
-   drivetrain: String,
-   handlebars: String,
-   saddle: String,
-   frontWheel: String,
-   rearWheel: String,
-   images: [{
-      name: String,
-      url: String
-   }],
-   instagram: String
-}, { minimize: false });
+    info: {
+        title: String,
+        instagram: String
+    },
+    specifications: {
+        frame: String,
+        fork: String,
+        cranks: String,
+        pedals: String,
+        drivetrain: String,
+        handlebars: String,
+        saddle: String,
+        frontWheel: String,
+        rearWheel: String
+    },
+    images: [{
+        name: String,
+        url: String
+    }]
+}, {minimize: false});
 
 
 BikeSchema.plugin(mongooseApiQuery);
-BikeSchema.plugin(createdModified, { index: true });
+BikeSchema.plugin(createdModified, {index: true});
 
 var Bike = mongoose.model('Bike', BikeSchema);
 module.exports = Bike;
--- routes/bikes.js
+++ routes/bikes.js
@@ -2,80 +2,111 @@
 
 var errors = require('restify-errors');
 var Bike = require('../models/bike');
+var User = require('../models/user');
 
 server.get('/bikes/', function (req, res, next) {
-   Bike.apiQuery(req.params, function (error, docs) {
-      if (error) {
-         console.error(error);
-         return next(new errors.InvalidContentError(error.errors.name.message));
-      }
+    Bike.apiQuery(req.params, function (error, docs) {
+        if (error) {
+            console.error(error);
+            return next(new errors.InvalidContentError(error.errors.name.message));
+        }
 
-      res.send(docs);
-      next();
-   })
+        res.send(docs);
+        next();
+    })
 });
 
-server.get('/bikes/:handle', function(req, res, next) {
-   Bike.apiQuery({instagram: req.params.handle}, function(err, docs) {
+server.get('/bikes/:handle', function (req, res, next) {
+    Bike.apiQuery({instagram: req.params.handle}, function (err, docs) {
 
-      if (err) {
-         log.error(err);
-         return next(new errors.InvalidContentError(err.errors.name.message))
-      }
+        if (err) {
+            log.error(err);
+            return next(new errors.InvalidContentError(err.errors.name.message))
+        }
 
-      res.send(docs);
-      next();
-   })
+        res.send(docs);
+        next();
+    })
 
 });
 
 server.get('/bike/:bike_id', function (req, res, next) {
-   Bike.findOne({ _id: req.params.bike_id }, function (error, document) {
-      if (error) {
-         log.error(error);
-         return next(new errors.InvalidContentError(error.errors.name.message));
-      }
+    Bike.findOne({_id: req.params.bike_id}, function (error, document) {
+        if (error) {
+            log.error(error);
+            return next(new errors.InvalidContentError(error.errors.name.message));
+        }
 
-      res.send(document);
-      next();
-   })
+        res.send(document);
+        next();
+    })
 });
 
-server.post('/bike', function(req, res, next) {
-   var data = req.body || {};
-   var bike = new Bike(data);
-   bike.save(function(err) {
+server.post('/bike', function (req, res, next) {
+    var data = req.body || {};
+    var bike = new Bike(data);
+    bike.save(function (err) {
 
-      if (err) {
-         log.error(err);
-         return next(new errors.InternalError(err.message));
-      }
+        if (err) {
+            log.error(err);
+            return next(new errors.InternalError(err.message));
+        }
 
-      res.send(201);
-      next();
-   })
+        res.send(201);
+        next();
+    })
 });
 
 server.put('/bike/:bike_id', function (req, res, next) {
-   var data = req.body || {};
+    var data = req.body || {};
 
-   Bike.findOne({_id: req.params.bike_id}, function (error, doc) {
-      if(error) {
-         log.error(error);
-         return next(new errors.InvalidContentError(err.errors.name.message));
-      }
-      else if(!doc) {
-         return next(new errors.ResourceNotFoundError('The resource you requested could not be found.'));
-      }
-
-      Bike.update({ _id: req.params.bike_id }, data, function(error) {
-         if (error) {
+    Bike.findOne({_id: req.params.bike_id}, function (error, doc) {
+        if (error) {
             log.error(error);
-            return next(new errors.InvalidContentError(error.errors.name.message))
-         }
+            return next(new errors.InvalidContentError(err.errors.name.message));
+        }
+        else if (!doc) {
+            return next(new errors.ResourceNotFoundError('The resource you requested could not be found.'));
+        }
 
-         res.send(200, data);
-         next();
-      })
-   })
+        Bike.update({_id: req.params.bike_id}, data, function (error) {
+            if (error) {
+                log.error(error);
+                return next(new errors.InvalidContentError(error.errors.name.message))
+            }
+
+            res.send(200, data);
+            next();
+        })
+    })
+});
+
+server.del('/bike/delete/:bike_id', function (req, res, next) {
+    var token = req.headers['x-fista-authenticate'] || {};
+    if (!token) {
+        res.send(400);
+    }
+
+    var credentials = global.atob(token).split(':');
+    User.findOne({ username: credentials[0] }, function (userError, user) {
+        if (userError) {
+            log.error(userError);
+            return next(new errors.InvalidContentError(userError.errors.name.message));
+        } else if(!user) {
+            return next(new errors.ResourceNotFoundError('The resource you requested could not be found.'))
+        } else {
+            Bike.findOne({_id: req.params.bike_id}, function (bikeError, bike) {
+                if (bikeError) {
+                    log.error(bikeError);
+                    return next(new errors.InvalidContentError(bikeError.errors.name.message));
+                }
+
+                if (bike && (user.instagram === bike.info.instagram || user.instagram.bike.info.instagram)) {
+                    bike.remove();
+                    res.send(204);
+                    next();
+                }
+            })
+        }
+    });
 });
